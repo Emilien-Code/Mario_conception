cff7f380de125b037d1257331cfc97a933db9406
Emilien <emilien.ginko@gmail.com>
2022-10-17 17:11:21 +0200

    🔧 Blocs

---
+++ src/game/Blocs/Mystery.ts
@@ -0,0 +1,27 @@
+import Bloc from "./Bloc"
+import IHitableBlock from "../utils/IHitableBlock"
+import Position from "../utils/Position"
+
+class Bricks extends Bloc implements IHitableBlock{
+    private isOpened: boolean;
+
+
+    constructor(position: Position){
+        super(position);
+        this.isOpened = false;
+    }
+
+    hit(): void {
+        this.isOpened = true;
+        console.log("Bricks disapear")
+    }
+
+    render(){
+        if(!this.isOpened)
+            return "?"
+        return "";
+    }   
+    
+}
+
+export default Bricks
--- src/game/Game.ts
+++ src/game/Game.ts
@@ -2,7 +2,8 @@ const screenWidth = 10;
 const screenHeight = 10;
 
 class Game {
-    square : Array<Array<string>>; 
+    square : Array<Array<any>>; 
+
 
     constructor(){
         this.square = [];
@@ -10,13 +11,12 @@ class Game {
     }
 
     init(): void{
-        console.log(this.square)
         for(let i:number = 0; i < screenWidth; i++){
             
-            const a:Array<string> =[];
+            const a:Array<any> =[];
             
             for(let j:number = 0; j < screenWidth; j++){
-                a.push("#");
+                a.push(0);
             }
 
             this.square.push(a);
@@ -31,9 +31,11 @@ class Game {
 
     render(): void{
         for(let i:number = 0; i < screenWidth; i++){
-            for(let j:number = 0; j < screenWidth; j++){
-                process.stdout.write(this.square[i][j]);
-            }   
+            //for(let j:number = 0; j < screenWidth; j++){
+
+                // checker avec des typeof le remettre dans un tab et le console.log
+                console.log(this.square[i]/*?.render* In the future*/);
+            //}   
             console.log("");
         }
     }
